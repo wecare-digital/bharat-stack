8a25c0bab9d4a6ccc1317513871dfdb96d115ab1
WECARE.DIGITAL <base@wecare.digital>
2026-03-03 15:26:44 +0530

    feat: use clean SVG plus icon and improve tag menu positioning for all rows

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -737,18 +737,17 @@ const Contacts: React.FC<PageProps> = ({ signOut, user }) => {
                                     alignItems: 'center', 
                                     justifyContent: 'center', 
                                     padding: 0, 
-                                    flexShrink: 0,
-                                    fontSize: 18,
-                                    fontWeight: 600,
-                                    lineHeight: 1
+                                    flexShrink: 0
                                   } as React.CSSProperties}
                                 >
-                                  +
+                                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8 3.33334V12.6667M3.33334 8H12.6667" stroke="#059669" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                  </svg>
                                 </button>
                                 {showTagMenu === c.contactId && (
                                   <div style={{ 
                                     position: 'absolute', 
-                                    ...(rowIndex < 3 ? { top: '100%', marginTop: 4 } : { bottom: '100%', marginBottom: 4 }),
+                                    ...(rowIndex < Math.floor(paginatedContacts.length / 2) ? { top: '100%', marginTop: 4 } : { bottom: '100%', marginBottom: 4 }),
                                     left: 0, 
                                     background: '#fff', 
                                     border: '2px solid #D1FAE5', 
